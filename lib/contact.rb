f8d763411c5f6a136841d7f586554fe3398c9db1
Mike Goren <mgoren@mortalwombat.net>
2015-01-16 09:42:19 -0800

    add method to return array of all names in address book

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,14 +1,25 @@
 class Contact
+	@@contacts = []
 
-attr_reader(:name, :number)
+	attr_reader(:name, :number)
 
-define_method(:initialize) do |attributes|
-	@name = attributes.fetch(:name)
-	@number = attributes.fetch(:number)
-end
+	define_method(:initialize) do |attributes|
+		@name = attributes.fetch(:name)
+		@number = attributes.fetch(:number)
+		@@contacts.push(self)
+	end
 
+	define_singleton_method(:list_all_names) do
+		names = []
+		@@contacts.each() do |contact|
+			names.push(contact.name())
+		end
+		names
+	end
 
-
+	define_singleton_method(:clear) do
+		@@contacts = []
+	end
 
 
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,19 +3,33 @@ require('contact')
 
 describe(Contact) do
 
+	before() do
+		Contact.clear()
+	end
+
 	describe('#number') do
 		it("returns number when called on a Contact") do
-			me = Contact.new({ :name => "Mike", :number => "314-159-2653"})
+			me = Contact.new({ :name => "Mike", :number => "314-159-2653" })
 			expect(me.number()).to(eq("314-159-2653"))
 		end
 	end	
 
 	describe('#name') do
 		it("returns name when called on a Contact") do
-			me = Contact.new({ :name => "Mike", :number => "314-159-2653"})
+			me = Contact.new({ :name => "Mike", :number => "314-159-2653" })
 			expect(me.name()).to(eq("Mike"))
 		end
 	end	
+
+	describe('.list_all_names') do
+		it("lists all contacts' names") do
+			me = Contact.new({ :name => "Mike", :number => "314-159-2653" })
+			friend = Contact.new({ :name => "Anna", :number => "123-456-7890" })
+			friend2 = Contact.new({ :name => "Dan", :number => "222-222-2222" })
+			expect(Contact.list_all_names()).to(eq(["Mike", "Anna", "Dan"]))
+		end
+	end
+
 
 
 end
