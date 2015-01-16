eeefafad487088ff53102f02405f8541895efff8
Mike Goren <mgoren@mortalwombat.net>
2015-01-16 09:36:05 -0800

    correctly returns name and number when called on Contact object

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,11 +3,19 @@ require('contact')
 
 describe(Contact) do
 
-	describe('#name') do
-		it("returns name when called on a Contact") do
+	describe('#number') do
+		it("returns number when called on a Contact") do
 			me = Contact.new({ :name => "Mike", :number => "314-159-2653"})
 			expect(me.number()).to(eq("314-159-2653"))
 		end
 	end	
+
+	describe('#name') do
+		it("returns name when called on a Contact") do
+			me = Contact.new({ :name => "Mike", :number => "314-159-2653"})
+			expect(me.name()).to(eq("Mike"))
+		end
+	end	
+
 
 end
